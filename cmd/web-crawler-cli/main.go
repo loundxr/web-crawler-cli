7d193e0b5c1599127a77093d48581036446372ca
Andrei Litvinovich <loounderr@gmail.com>
2026-09-23 13:45:51 +0300

    feat: add page parser

--- cmd/web-crawler-cli/main.go
+++ cmd/web-crawler-cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/loundxr/web-crawler-cli/internal/config"
 	"github.com/loundxr/web-crawler-cli/internal/fetcher"
+	"github.com/loundxr/web-crawler-cli/internal/parser"
 )
 
 func main() {
@@ -23,10 +25,11 @@ func main() {
 	// fetcher test
 	f := fetcher.New(cfg.MaxConcurrency)
 	outcome := f.Fetch(ctx, cfg.StartURLs[0])
+	data := make([]byte, 0)
 
 	if outcome.Body != nil {
 		defer outcome.Body.Close()
-		data, err := io.ReadAll(outcome.Body)
+		data, err = io.ReadAll(outcome.Body)
 		if err != nil {
 			log.Fatal("read body error:", err)
 		}
@@ -40,4 +43,14 @@ func main() {
 		outcome.Err,
 	)
 	// fetcher test
+	//parser test
+
+	page, err := parser.ParseResponseBody(bytes.NewReader(data), cfg.StartURLs[0])
+	if err != nil {
+		log.Fatal("parse body error:", err)
+	}
+
+	fmt.Printf("title: %s\n", page.Title)
+	fmt.Printf("raw links: %v\n", page.RawLinks)
+	//parser test
 }
