7cb419747d9079ee6fe421d1e5aba79981a2ec88
Andrei Litvinovich <loounderr@gmail.com>
2026-09-24 03:02:58 +0300

    style: remove unnecessary comments

--- cmd/web-crawler-cli/main.go
+++ cmd/web-crawler-cli/main.go
@@ -50,39 +50,6 @@ func main() {
 	log.Println("crawling completed successfully")
 }
 
-// // fetcher test
-// f := fetcher.New(cfg.MaxConcurrency)
-// outcome := f.Fetch(ctx, cfg.StartURLs[0])
-// data := make([]byte, 0)
-
-// if outcome.Body != nil {
-// 	defer outcome.Body.Close()
-// 	data, err = io.ReadAll(outcome.Body)
-// 	if err != nil {
-// 		log.Fatal("read body error:", err)
-// 	}
-// 	fmt.Printf("body: %s\n", string(data))
-// }
-
-// fmt.Printf(
-// 	"code: %d\nskip reason: %s\nerror: %v\n",
-// 	outcome.StatusCode,
-// 	outcome.SkipReason,
-// 	outcome.Err,
-// )
-// // fetcher test
-// //parser test
-
-// page, err := parser.ParseResponseBody(bytes.NewReader(data), cfg.StartURLs[0])
-// if err != nil {
-// 	log.Fatal("parse body error:", err)
-// }
-
-// fmt.Printf("title: %s\n", page.Title)
-// fmt.Printf("raw links: %v\n", page.RawLinks)
-// //parser test
-// }
-
 func writeResultToFile(nodes []*model.Node, path string) error {
 	data, err := json.MarshalIndent(nodes, "", "  ")
 	if err != nil {
